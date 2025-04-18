504583edb6e12206967eb49033c630247e35b978
hyowony108 <hyowony108@gmail.com>
2025-04-18 17:45:14 +0900

    비동기 요청

--- todoList/src/main/java/edu/kh/todo/model/service/TodoService.java
+++ todoList/src/main/java/edu/kh/todo/model/service/TodoService.java
@@ -1,5 +1,6 @@
 package edu.kh.todo.model.service;
 
+import java.util.List;
 import java.util.Map;
 
 import edu.kh.todo.model.dto.Todo;
@@ -25,8 +26,41 @@ public interface TodoService {
 
 	/** 할 일 상세조회
 	 * @param todoNo
-	 * @return Todo
+	 * @return todo
 	 */
 	Todo todoDetail(int todoNo);
+
+	/** 완료 여부 변경
+	 * @param todo
+	 * @return result
+	 */
+	int changeComplete(Todo todo);
+
+	/** 할 일 수정
+	 * @param todo
+	 * @return result
+	 */
+	int todoUpdate(Todo todo);
+
+	/** 할 일 삭제
+	 * @param todoNo
+	 * @return result
+	 */
+	int todoDelete(int todoNo);
+
+	/** 전체 할 일 개수 조회
+	 * @return totalCount
+	 */
+	int getTotalCount();
+
+	/** 완료된 할 일 개수 조회
+	 * @return completeCount
+	 */
+	int getCompleteCount();
+
+	/** 할 일 목록 조회
+	 * @return todoList
+	 */
+	List<Todo> selectList();
 
 }
